231e5ecec4c7930f8e761cf381bcaa095c89eccc
JamesDev292 <james.barthee@orange.fr>
2024-12-03 13:00:13 +0100

    add gem pg_search & add model crypto

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -1,10 +1,17 @@
 class CryptosController < ApplicationController
-    def index
-        @crypto_search = Crypto.all 
-
-
-
+  def index
+    if params[:search].present?
+      # Filtrer les cryptos par nom (ou autre champ)
+      @crypto_search = Crypto.where("name LIKE ?", "%#{params[:search]}%")
+    else
+      @crypto_search = Crypto.limit(5)
     end
+
+    respond_to do |format|
+      format.html  # Pour les requêtes classiques
+      format.js    # Pour les requêtes AJAX
+    end
+  end
 
 
 
--- app/models/crypto.rb
+++ app/models/crypto.rb
@@ -2,4 +2,12 @@ class Crypto < ApplicationRecord
   has_many :votes
   has_many :users, through: :votes
   has_many :posts
+
+  include PgSearch::Model
+
+  pg_search_scope :search_by_name_or_symbol,
+    against: [:name, :symbol],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
