03ff69d87602cb24d4611edc2c55bbf17db08aa6
Florian-THP <triboutflorian@outlook.com>
2024-12-03 18:06:18 +0100

    add Chart.js and code chart time, and code html view Crypto

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -1,8 +1,11 @@
 class CryptosController < ApplicationController
     def index
-
+       @cryptos = Crypto.all
     end
 
+    def show
+      @crypto = Crypto.find(params[:id])
+    end
 
     
 end
