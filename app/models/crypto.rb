dec68a2e3ad4e60c1cb1a786a31652d8aa50b5a7
Alexe Marichal <marichalalexe@gmail.com>
2024-12-03 12:12:12 +0200

    021-add-validations-in-post-comment-crypto-models

--- app/models/crypto.rb
+++ app/models/crypto.rb
@@ -1,5 +1,9 @@
 class Crypto < ApplicationRecord
-  has_many :votes
+  has_many :votes, dependent: :destroy
   has_many :users, through: :votes
-  has_many :posts
+  has_many :posts, dependent: :destroy
+
+  validates :name, :symbol, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+
 end
