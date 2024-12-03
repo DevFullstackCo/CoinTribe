86c77fdf36c8ad16b6bda45e06bcc4bbdc09e19b
Alexe Marichal <marichalalexe@gmail.com>
2024-12-03 10:19:23 +0200

    001-migration-changes-bigint-in-crypto-table

---
+++ db/migrate/20241203081651_change_integer_to_big_int_in_cryptos.rb
@@ -0,0 +1,6 @@
+class ChangeIntegerToBigIntInCryptos < ActiveRecord::Migration[8.0]
+  def change
+    change_column :cryptos, :volume_24h, :bigint
+    change_column :cryptos, :market_cap, :bigint
+  end
+end
