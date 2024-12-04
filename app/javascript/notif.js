0401e9e131181e02f6f7179ffdcdb06f64185931
Florian-THP <triboutflorian@outlook.com>
2024-12-04 17:10:04 +0100

    code css to index crypto , navbar and profil menu

--- app/javascript/notif.js
+++ app/javascript/notif.js
@@ -1,22 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const notifBtn = document.querySelector('.btn-notif');
-    const notifPopup = document.querySelector('.notif-popup');
-  
-    notifBtn.addEventListener('click', function() {
-      if (notifPopup.style.display === 'block') {
-        notifPopup.style.display = 'none';
-      } else {
-        notifPopup.style.display = 'block';
-        const rect = notifBtn.getBoundingClientRect();
-        notifPopup.style.top = rect.bottom + 'px';
-        notifPopup.style.left = rect.left + 'px';
-      }
-    });
-  
-    document.addEventListener('click', function(event) {
-      if (!notifBtn.contains(event.target) && !notifPopup.contains(event.target)) {
-        notifPopup.style.display = 'none';
-      }
-    });
+  const notifBtn = document.querySelector('.btn-notif');
+  const notifPopup = document.querySelector('.notif-popup');
+
+  notifBtn.addEventListener('click', function(event) {
+    event.stopPropagation(); // Empêche la propagation de l'événement pour éviter de fermer la popup immédiatement
+
+    if (notifPopup.style.display === 'block') {
+      notifPopup.style.display = 'none';
+    } else {
+      notifPopup.style.display = 'block';
+      const rect = notifBtn.getBoundingClientRect();
+      notifPopup.style.top = rect.bottom + 'px';
+      notifPopup.style.left = rect.left - 200 + 'px';
+    }
   });
-  
+
+  // Ferme la popup si l'utilisateur clique à l'extérieur
+  document.addEventListener('click', function(event) {
+    if (!notifBtn.contains(event.target) && !notifPopup.contains(event.target)) {
+      notifPopup.style.display = 'none';
+    }
+  });
+});
