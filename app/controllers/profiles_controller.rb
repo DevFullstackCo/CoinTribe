a2da1027dffb7e32958093ed8585d12bb7bf0b38
CarolineOlivier <linepro.olivier@gmail.com>
2024-12-04 22:02:43 +0100

    Saved progress on profile page: added delete account functionality with form_with

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,24 +5,6 @@ class ProfilesController < ApplicationController
       @user = current_user
     end
   
-    def edit
-      @user = current_user
-    end
-  
-    def update
-      @user = current_user
-      if @user.update(user_params)
-        redirect_to profile_path, notice: 'Profile updated successfully!'
-      else
-        render :edit, status: :unprocessable_entity
-      end
-    end
-  
-    def destroy
-      current_user.destroy
-      redirect_to root_path, notice: 'Your account has been deleted.'
-    end
-  
     private
   
     def user_params
