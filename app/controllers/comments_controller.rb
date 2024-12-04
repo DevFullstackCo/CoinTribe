ed4b2afffc84b12926a6f0282a3bf0cbef5badca
Alexe Marichal <marichalalexe@gmail.com>
2024-12-04 17:40:56 +0200

    add method destroy to comment controller + translate message content in english for controller post and comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,16 +8,20 @@ class CommentsController < ApplicationController
     @comment.user = current_user
 
     if @comment.save
-      redirect_to crypto_path(@crypto), notice: "Votre commentaire a été ajouté avec succès !"
+      redirect_to crypto_path(@crypto), notice: "Your comment has been successfully added!"
     else
       @comments = @post.comments.order(created_at :desc)
       @posts = @crypto.posts.order(created_at: :desc)
-      flash.now[:alert] = "Erreur lors de la création de votre commentaire."
+      flash.now[:alert] = "An error occurred while creating your comment. Please try again."
       render "cryptos/show"
     end
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    @crypto = @comment.post.crypto
+    @comment.destroy
+    redirect_to crypto_path(@crypto), notice: "Your comment has been successfully deleted"
 
   end
 
