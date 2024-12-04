c6bf130351af7360264f363680e63aa6b10475c6
Alexe Marichal <marichalalexe@gmail.com>
2024-12-04 10:11:58 +0200

    method create for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,30 @@
 class CommentsController < ApplicationController
+
+  def create
+    @crypto = Crypto.find(params[:crypto_id])
+    @post = @crypto.post
+    @comment = Comment.new(comment_params)
+    @comment.post = @post
+    @commeht.user = current_user
+
+    if @comment.save
+      redirect_to crypto_path(@crypto)
+    else
+      
+    end
+
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
 end
+
