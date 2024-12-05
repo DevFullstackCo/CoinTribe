6f2008dea579395501a9668964bea4a1d40e64e0
CarolineOlivier <linepro.olivier@gmail.com>
2024-12-05 15:52:51 +0100

    Removal of user_params in ProfilesController

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,11 +4,5 @@ class ProfilesController < ApplicationController
     def show
       @user = current_user
     end
-  
-    private
-  
-    def user_params
-      params.require(:user).permit(:username, :email, :avatar, :is_subscriber, :password, :password_confirmation)
-    end
   end
   
