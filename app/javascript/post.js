c8c92e4d1417c32e3f5fc92d1daef1b542f7565f
Florian-THP <triboutflorian@outlook.com>
2024-12-06 14:43:44 +0100

    add js menu

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -1,30 +1,61 @@
 document.addEventListener('turbo:load', () => {
-    const postsContainer = document.querySelector('#posts_container');
-  
-    if (postsContainer) {
-      postsContainer.addEventListener('click', (e) => {
-        const post = e.target.closest('.post');
+  const postsContainer = document.querySelector('#posts_container');
+
+  if (postsContainer) {
+    postsContainer.addEventListener('click', (e) => {
+      const post = e.target.closest('.post');
+      
+      if (post) {
+        const commentLine = post.querySelector('.comment-line');
+        const newCommentInput = post.querySelector('.new-comment');
+
+        if (commentLine) {
+          if (newCommentInput && newCommentInput.contains(e.target)) {
+            return;
+          }
+
         
-        if (post) {
-          const commentLine = post.querySelector('.comment-line');
-          const newCommentInput = post.querySelector('.new-comment');
-  
-          if (commentLine) {
-            if (newCommentInput && newCommentInput.contains(e.target)) {
-              return;
-            }
-  
-            commentLine.classList.toggle('visible');
-            commentLine.classList.toggle('hidden');
-  
-            if (commentLine.classList.contains('visible')) {
-              post.classList.add('shadow');
-            } else {
-              post.classList.remove('shadow');
-            }
+          commentLine.classList.toggle('visible');
+          commentLine.classList.toggle('hidden');
+
+          if (commentLine.classList.contains('visible')) {
+            post.classList.add('shadow');
+          } else {
+            post.classList.remove('shadow');
           }
         }
-      });
-    }
-  });
+      }
+    });
+  }
+});
+
+
+document.addEventListener('turbo:frame-load', () => {
+  const postsContainer = document.querySelector('#posts_container');
   
+  if (postsContainer) {
+    postsContainer.addEventListener('click', (e) => {
+      const post = e.target.closest('.post');
+      
+      if (post) {
+        const commentLine = post.querySelector('.comment-line');
+        const newCommentInput = post.querySelector('.new-comment');
+
+        if (commentLine) {
+          if (newCommentInput && newCommentInput.contains(e.target)) {
+            return;
+          }
+
+          commentLine.classList.toggle('visible');
+          commentLine.classList.toggle('hidden');
+
+          if (commentLine.classList.contains('visible')) {
+            post.classList.add('shadow');
+          } else {
+            post.classList.remove('shadow');
+          }
+        }
+      }
+    });
+  }
+});
