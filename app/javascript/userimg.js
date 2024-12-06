9c4a2811842439885de9a4b25477f7caa56133ee
Florian-THP <triboutflorian@outlook.com>
2024-12-06 18:22:26 +0100

    add css to profiles

---
+++ app/javascript/userimg.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const image = document.getElementById('image-user');
+    const popup = document.getElementById('popup');
+    const closeBtn = document.getElementById('close');
+
+    if (image) {
+        image.addEventListener('click', function() {
+            popup.style.display = 'flex';
+        });
+    }
+
+    if (closeBtn) {
+        closeBtn.addEventListener('click', function() {
+            popup.style.display = 'none';
+        });
+    }
+
+    window.addEventListener('click', function(event) {
+        if (event.target === popup) {
+            popup.style.display = 'none';
+        }
+    });
+});
