58d7698b47b27792092717580e4e06354d353ed2
JamesDev292 <james.barthee@orange.fr>
2024-12-09 20:27:35 +0100

    add ContactMailer

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,22 @@
+class ContactMailer < ApplicationMailer
+
+def welcome_email(user)
+    @user = user
+    
+
+    mail(
+      to: @user.email, 
+      subject: 'Welcome to our home !'
+      ) 
+  end
+
+  def send_contact_email(admin_email, subject, message, user_email)
+    @message = message
+    @user_email = user_email
+
+    mail(
+      to: admin_email,
+      subject: subject
+    )
+  end
+end
