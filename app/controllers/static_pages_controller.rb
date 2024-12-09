69505916c8531523b952f395ec4b67c41a9d719c
JamesDev292 <james.barthee@orange.fr>
2024-12-09 20:43:40 +0100

    add send mailer contact

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,23 @@ class StaticPagesController < ApplicationController
 
   def privacy_policy
   end
+
+  def send_message
+    subject = params[:subject]
+    message = params[:message]
+  
+    admin_emails = User.where(is_admin: true).pluck(:email)
+  
+    if admin_emails.any?
+      admin_emails.each do |admin_email|
+        ContactMailer.send_contact_email(admin_email, subject, message, current_user.email).deliver_now
+      end
+      flash[:notice] = "Votre message a été envoyé avec succès."
+    else
+      flash[:alert] = "Aucun administrateur n'est disponible pour recevoir votre message."
+    end
+  
+    redirect_to contact_path
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get 'cgu', to: 'static_pages#cgu'
   get "privacy_policy", to: "static_pages#privacy_policy"
-  
+  get 'contact', to: 'static_pages#contact'
+  post 'contact/send_message', to: 'static_pages#send_message'
+
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
