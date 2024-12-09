32059cfb4f1740e5a6014f141c9b58666a13f1a2
CarolineOlivier <linepro.olivier@gmail.com>
2024-12-09 15:30:00 +0100

    Feature #071: Added username update functionality with corrections to controllers, HTML, CSS, JS, models, and routes. Excluded password validation for username change.

--- app/javascript/userimg.js
+++ app/javascript/userimg.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("turbo:load", function() {
     const image = document.getElementById('image-user');
     const popup = document.getElementById('popup');
     const closeBtn = document.getElementById('close');
@@ -12,6 +12,24 @@ document.addEventListener("DOMContentLoaded", function() {
     if (closeBtn) {
         closeBtn.addEventListener('click', function() {
             popup.style.display = 'none';
+        });
+    }
+});
+
+document.addEventListener("turbo:load", function() {
+    const editIcon = document.getElementById('open-username-popup'); 
+    const usernamePopup = document.getElementById('username-popup'); 
+    const closeUsernamePopupBtn = document.getElementById('close-username-popup'); 
+
+    if (editIcon) {
+        editIcon.addEventListener('click', function() {
+            usernamePopup.style.display = 'flex';
+        });
+    }
+
+    if (closeUsernamePopupBtn) {
+        closeUsernamePopupBtn.addEventListener('click', function() {
+            usernamePopup.style.display = 'none';
         });
     }
 });
