174f5b78932ffdf626a0229a89eb65ce4242c56d
Alexe Marichal <marichalalexe@gmail.com>
2024-12-10 12:50:02 +0200

    erase destroy method + added if condition to force quantity to zero if is_favorite is false

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,6 +7,8 @@ class FavoritesController < ApplicationController
     @favorite.user = current_user
     @favorite.crypto = @crypto
 
+    adjust_quantity_for_favorite(@favorite)
+
     if @favorite.save
       flash[:notice] = "Crypto successfully added to your favorites!"
     else
@@ -16,43 +18,38 @@ class FavoritesController < ApplicationController
     redirect_to crypto_path(@crypto)
   end
 
+
   def update
     @crypto = Crypto.find(params[:crypto_id])
     @favorite = Favorite.find_by(user: current_user, crypto: @crypto)
 
-    logger.debug "Params reçus : #{params.inspect}" # Debug des paramètres
-    logger.debug "Favorite avant mise à jour : #{@favorite.inspect}" # Debug de l'objet avant mise à jour
+    @favorite.assign_attributes(favorites_params)
+    adjust_quantity_for_favorite(@favorite)
 
-    if @favorite&.update(favorites_params)
-      logger.debug "Favorite après mise à jour : #{@favorite.inspect}"
+    if @favorite.save
       flash[:notice] = "Favorite successfully updated!"
     else
-      logger.debug "Erreurs lors de la mise à jour : #{@favorite.errors.full_messages}"
       flash[:alert] = "Could not update favorite."
     end
 
     redirect_to crypto_path(params[:crypto_id])
   end
 
+
   def edit
     @crypto = Crypto.find(params[:crypto_id])
     @favorite = Favorite.find_by(user: current_user, crypto: @crypto) || Favorite.new(crypto: @crypto, user: current_user)
   end
 
-  def destroy
-    @favorite = Favorite.find(params[:id])
-
-    if @favorite.destroy
-      redirect_to crypto_path(@favorite.crypto), notice: "Removed from favorites!"
-    else
-      redirect_to crypto_path(@favorite.crypto), alert: "Could not remove favorite."
-    end
-  end
-
+  
   private
 
   def favorites_params
     params.require(:favorite).permit(:quantity, :is_favorite)
+  end
+
+  def adjust_quantity_for_favorite(favorite)
+    favorite.quantity = 0 unless favorite.is_favorite
   end
   
 end
