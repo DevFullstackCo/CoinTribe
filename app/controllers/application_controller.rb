535dfe1800447b11d589b4facc6f2d0791f49fe1
Alexe Marichal <marichalalexe@gmail.com>
2024-12-10 15:20:39 +0200

    add before_action in Application controller to load favorite cryptos in every view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  private
+
+  def set_user_favorites
+    if user_signed_in?
+      @favorites_for_menu = current_user.favorites.where(is_favorite: true).includes(:crypto)
+    end
+  end
 end
