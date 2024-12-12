ab26c4bca6f55086cc6d32ecd9551dc3e562f8dd
Alexe Marichal <marichalalexe@gmail.com>
2024-12-12 11:59:01 +0200

    added method to turn notifications as read + button to call the method

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,9 @@
 class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
+
+  def mark_as_read
+    current_user.notifications.where(is_read?: false).update_all(is_read?: true)
+  end
+
 end
