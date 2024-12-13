9d01844b8196d935f846293fb6a1769d64b42282
Dev-Moulin <p.moulin.95@gmail.com>
2024-12-13 19:20:58 +0100

    change name of cgu

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
 
   get "up" => "rails/health#show", as: :rails_health_check
-  get 'termsofuse', to: 'static_pages#cgu'
+  get 'termsofuse', to: 'static_pages#termsofuse'
   get 'faq', to: 'static_pages#faq'
   get "privacy_policy", to: "static_pages#privacy_policy"
   get 'contact', to: 'static_pages#contact'
